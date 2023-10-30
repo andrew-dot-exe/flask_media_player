0ee3edfae19d793ddc33e3b481ce6d3679920d96
andrewexe <andrejnikitin974@gmail.com>
2023-10-30 13:15:24 +0400

    start developing

---
+++ backend/mpdcontrol.py
@@ -0,0 +1,11 @@
+class MPDMusicPlayer():
+    def __init__(self) -> None:
+        pass
+    
+    def play_file(self, filename : str) -> None:
+        pass
+    
+    def set_library_directory(self, dir_name) -> None:
+        pass
+    
+    
